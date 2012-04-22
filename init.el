30faf4c3e08e007042c564fd2e5f7719dd7ed876
Alessandro Campagni <alessandro@lilik.it>
2012-04-22 16:10:11 +0200

    cedet and ecb working

---
+++ init.el
@@ -0,0 +1,28 @@
+;;;; ECB
+
+(load-file "~/.emacs.d/include/cedet-1.0.1/common/cedet.el")
+;;(global-ede-mode 1)                      ; Enable the Project management system
+;;(semantic-load-enable-code-helpers)      ; Enable prototype help and smart completion 
+;;(global-srecode-minor-mode 1)            ; Enable template insertion menu
+
+(add-to-list 'load-path "~/.emacs.d/include/ecb-snap")
+(require 'ecb)
+
+(custom-set-variables
+  ;; custom-set-variables was added by Custom.
+  ;; If you edit it by hand, you could mess it up, so be careful.
+  ;; Your init file should contain only one such instance.
+  ;; If there is more than one, they won't work right.
+ '(ecb-auto-activate t)
+ '(ecb-auto-compatibility-check nil)
+ '(ecb-layout-name "ecb-layout-directories.methods")
+ '(ecb-show-sources-in-directories-buffer (quote always))
+ '(ecb-source-path (quote ("~/")))
+ '(ecb-tip-of-the-day nil)
+ '(ecb-version-check nil))
+(custom-set-faces
+  ;; custom-set-faces was added by Custom.
+  ;; If you edit it by hand, you could mess it up, so be careful.
+  ;; Your init file should contain only one such instance.
+  ;; If there is more than one, they won't work right.
+ )
