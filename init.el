04500d5bc40658b4a73536c83c4cb37d857ee595
Alessandro Campagni <alessandro@lilik.it>
2012-04-28 16:53:26 +0200

    autostart rsens auto complete after . or ::

--- init.el
+++ init.el
@@ -132,4 +132,15 @@
 (add-to-list 'load-path (concat rsense-home "/etc"))
 (require 'rsense)
 
+;; Complete by C-c .
+;; (add-hook 'ruby-mode-hook
+;;           (lambda ()
+;;             (local-set-key (kbd "C-c .") 'ac-complete-rsense)))
+
+(add-hook 'ruby-mode-hook
+          (lambda ()
+            (add-to-list 'ac-sources 'ac-source-rsense-method)
+            (add-to-list 'ac-sources 'ac-source-rsense-constant)))
+
+
 ;;;; RSENSE end
