1800f8450492450cf5276c4b181c021ed2864e80
Alessandro Campagni <alessandro@lilik.it>
2012-05-11 19:22:15 +0200

    gitignore for .#init.el (emacs server stuff)

--- init.el
+++ init.el
@@ -224,7 +224,8 @@
 
 ;;;; ZENCODING
 
-(add-to-list 'load-path "~/emacs.d/inlcude/zencoding")
+(add-to-list 'load-path "~/.emacs.d/include/zencoding")
 (require 'zencoding-mode)
+(add-hook 'sgml-mode-hook 'zencoding-mode) ;; Auto-start on any markup modes
 
 ;;;; ZENCODING end
